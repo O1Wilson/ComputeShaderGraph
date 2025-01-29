a20bb6ba4eeb79d10f5508103c3d60cbf68e7cbb
O1Wilson <Badgermouse583@gmail.com>
2025-01-29 09:40:36 -0600

    Added Procedual Gradient Coloring to Fractal

--- My project/Assets/Point/Fractal Surface GPU.shader
+++ My project/Assets/Point/Fractal Surface GPU.shader
@@ -1,9 +1,4 @@
 Shader "Fractal/Fractal Surface GPU" {
-
-	Properties {
-		_BaseColor ("Base Color", Color) = (1.0, 1.0, 1.0, 1.0)
-		_Smoothness ("Smoothness", Range(0,1)) = 0.5
-	}
 	
 	SubShader {
 		CGPROGRAM
@@ -19,12 +14,11 @@ Shader "Fractal/Fractal Surface GPU" {
 			float3 worldPos;
 		};
 
-		float4 _BaseColor;
 		float _Smoothness;
 
 		void ConfigureSurface (Input input, inout SurfaceOutputStandard surface) {
-			surface.Albedo = _BaseColor.rgb;
-			surface.Smoothness = _Smoothness;
+			surface.Albedo = GetFractalColor().rgb;
+			surface.Smoothness = GetFractalColor().a;
 		}
 		ENDCG
 	}
--- My project/Assets/Point/FractalGPU.hlsl
+++ My project/Assets/Point/FractalGPU.hlsl
@@ -10,15 +10,37 @@ void ConfigureProcedural()
 		unity_ObjectToWorld._m10_m11_m12_m13 = m._m10_m11_m12_m13;
 		unity_ObjectToWorld._m20_m21_m22_m23 = m._m20_m21_m22_m23;
 		unity_ObjectToWorld._m30_m31_m32_m33 = float4(0.0, 0.0, 0.0, 1.0);
-#endif
+	#endif
 }
 
-void ShaderGraphFunction_float(float3 In, out float3 Out)
-{
-    Out = In;
+float4 _ColorA, _ColorB;
+float4 _SequenceNumbers;
+
+float4 GetFractalColor() {
+	#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+			float4 color;
+		color.rgb = lerp(
+			_ColorA.rgb, _ColorB.rgb,
+			frac(unity_InstanceID * _SequenceNumbers.x + _SequenceNumbers.y)
+		);
+		color.a = lerp(
+			_ColorA.a, _ColorB.a,
+			frac(unity_InstanceID * _SequenceNumbers.z + _SequenceNumbers.w)
+		);
+		return color;
+	#else
+		return _ColorA;
+	#endif
 }
 
-void ShaderGraphFunction_half(half3 In, out half3 Out)
+void ShaderGraphFunction_float(float3 In, out float3 Out, out float4 FractalColor)
 {
     Out = In;
+    FractalColor = GetFractalColor();
+}
+
+void ShaderGraphFunction_half(half3 In, out half3 Out, out float4 FractalColor)
+{
+    Out = In;
+    FractalColor = GetFractalColor();
 }
